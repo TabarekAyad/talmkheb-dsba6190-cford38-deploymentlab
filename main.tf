021b54a8f6e2369c63153a18dbd7bbdf5e89f5db
TabarekAyad <tabarek.ayad@gmail.com>
2024-11-13 20:02:16 -0500

    added requirements

--- main.tf
+++ main.tf
@@ -42,3 +42,30 @@ resource "azurerm_storage_account" "storage" {
 
   tags = local.tags
 }
+
+// Virtual Network
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-${var.class_name}-${var.student_name}-${var.environment}-${var.location}-${random_integer.deployment_id_suffix.result}"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+// Subnet
+
+resource "azurerm_subnet" "snet" {
+  name                 = "snet-${var.class_name}-${var.student_name}-${var.environment}-${var.location}-${random_integer.deployment_id_suffix.result}"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+  service_endpoints    = ["Microsoft.Sql", "Microsoft.Storage"]
+}
+
+// Network Rules
+
+ network_rules {
+    default_action             = "Deny"
+    ip_rules                   = ["100.0.0.1"]
+    virtual_network_subnet_ids = [azurerm_subnet.snet.id]
+  }
