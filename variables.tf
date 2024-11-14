48a439007a33499902c64bd4649d6ab2ebab08fb
Tabarek Ayad <tabarek.ayad@gmail.com>
2024-11-13 19:18:47 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -37,7 +37,7 @@ variable "environment" {
 variable "student_name" {
   description = "Application Name"
   type        = string
-  default     = "TAAAA"
+  default     = "t4"
 }
 
 variable "class_name" {
